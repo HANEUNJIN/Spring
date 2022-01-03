9efa2218ed59321fc8923f87772073bc1bafaea6
HANEUNJIN <20191281@vision.hoseo.edu>
2022-01-03 17:57:04 +0900

    first commit

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,15 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
+
+유저네임으로 설정할 거랑 이메일 좀 적어주세요 그 깃허브 이메일 ? 네ㅔ 20191281@vision.hoseo.edu 이거랑 유저네임은 그냥 이름으로 할게여 HANEUNJIN ㅋㅋㅋ 그 깃허브 이메일 저걸로 했어요?네네! 무슨 학생 인증하면 머 업그레이드 해준다는데?음
